334e204badf5b2d7697d314bd6f6b7e70b28b708
Jitterz <migraphics@hotmail.com>
2018-03-26 09:16:39 -0700

    Added unit upgrade panel

--- Super_Serious_Space_Game/assets/Scripts/MenuScripts/UnitCards/UnitCard.cs
+++ Super_Serious_Space_Game/assets/Scripts/MenuScripts/UnitCards/UnitCard.cs
@@ -6,22 +6,17 @@ using UnityEngine.UI;
 public class UnitCard : MonoBehaviour {
 
     public Sprite[] resourceSprites;
-    public Sprite[] attackTypes;
     public Sprite[] cardBackgrounds;
 
     public Image unitImage;
     public Image cardBackground;
     public Image resourceImage;
-    public Image attackImage;
 
     public Text unitName;
-    public Text attackDamage;
-    public Text attackSpeed;
-    public Text health;
+    public Text nickName;
     public Text unitCapacity;
-    public Text moveSpeed;
-    public Text buildTime;
     public Text resourceCost;
+    public Text cardLevel;
 
     // Use this for initialization
     void Start () {
