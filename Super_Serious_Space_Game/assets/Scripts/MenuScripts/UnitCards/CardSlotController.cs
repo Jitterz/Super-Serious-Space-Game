bca951d54be749b406be864d9ee871658b2ba077
Jitterz <migraphics@hotmail.com>
2018-04-07 12:01:22 -0700

    Drag and Drop Unit Cards - Card Slots

---
+++ Super_Serious_Space_Game/assets/Scripts/MenuScripts/UnitCards/CardSlotController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CardSlotController : MonoBehaviour, IDropHandler {
+
+    public GameObject unitCard
+    {
+        get
+        {
+            if (transform.childCount > 0)
+            {
+                return transform.GetChild(0).gameObject;
+            }
+            return null;
+        }
+    }
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        // if my card slot does not have an item then set the cards parent to the slot
+        if (!unitCard)
+        {
+            if (DragAndDropController.draggedObject != null)
+            {
+                DragAndDropController.draggedObject.transform.SetParent(transform);
+                DragAndDropController.draggedObject.transform.position = transform.position;
+            }
+        }
+    }
+}
---
+++ Super_Serious_Space_Game/assets/Scripts/MenuScripts/UnitCards/DragAndDropController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class DragAndDropController : MonoBehaviour, IBeginDragHandler, IDragHandler, IEndDragHandler {
+
+    public static GameObject draggedObject;
+    private Vector3 startPosition;
+    private Transform startingParent;
+    private Transform canvasParent;
+    private CanvasGroup canvasGroup;
+
+	// Use this for initialization
+	void Start () {
+        canvasGroup = GetComponent<CanvasGroup>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void OnBeginDrag(PointerEventData eventData)
+    {
+        startingParent = transform.parent;
+        canvasParent = GameObject.Find("Canvas").transform;
+        transform.SetParent(canvasParent);
+        draggedObject = gameObject;
+        startPosition = transform.position;
+        canvasGroup.blocksRaycasts = false;
+    }
+
+    public void OnDrag(PointerEventData eventData)
+    {
+        transform.position = Input.mousePosition;
+    }
+
+    public void OnEndDrag(PointerEventData eventData)
+    {
+        draggedObject = null;
+        canvasGroup.blocksRaycasts = true;
+        if (transform.parent == canvasParent)
+        {
+            transform.position = startPosition;
+            transform.SetParent(startingParent);            
+        }
+    }
+}
